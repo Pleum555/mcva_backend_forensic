4edbd891f4b405f7f67c3c423071e59fd46136dd
Witsaroot Jongsanguandee <89636629+Pleum555@users.noreply.github.com>
2024-03-04 18:51:54 +0700

    Add getActivitybyTest_Session(No have Student_ID)

--- src/controllers/s3Controller.ts
+++ src/controllers/s3Controller.ts
@@ -1,6 +1,6 @@
 // src/controllers/s3Controller.ts
 import express, { Router, Request, Response } from 'express';
-import { uploadToS3, readFileFromS3 } from '../services/s3service';
+import { s3service } from '../services/s3service';
 
 const s3Controller: Router = express.Router();
 
@@ -10,10 +10,10 @@ s3Controller.post('/upload', async (req: Request, res: Response) => {
     // Example JSON body for uploading: { "file_content": { "test": "test" }, "file_name": "test.txt" }
     // const Test_Session = req.body.Test_Session;
     // const Student_ID = req.body.Student_ID
-    const { Test_Session, Student_ID } = req.body
-
+    // Destructure and create a new object with only the desired properties
+    const { Test_Session, Student_ID, Name, Surname, ...Data } = req.body;
     // Assuming "file_content" is a JSON object and "file_name" is a string
-    const url = await uploadToS3(Test_Session, Student_ID, req.body);
+    const url = await s3service.uploadToS3(Test_Session, Student_ID, Name, Surname, Data);
 
     // Respond with the S3 URL after successful upload
     res.json({ success: true, url });
@@ -24,21 +24,21 @@ s3Controller.post('/upload', async (req: Request, res: Response) => {
 });
 
 // GET request to read a file from S3
-s3Controller.get('/read/:Test_Session/:Student_ID', async (req: Request, res: Response) => {
+s3Controller.get('/read/:Test_Session/:Student_ID?', async (req: Request, res: Response) => {
   try {
     // Extract the fileName parameter from the request path
     const { Test_Session, Student_ID } = req.params;
+    let fileContent;
 
-    // Example JSON body for reading: { "file": { "test": "test" }, "key": "test.txt" }
-    const fileContent = await readFileFromS3(Test_Session, Student_ID);
-
-    // Check if fileContent is undefined, indicating that the file was not found
-    if (fileContent === undefined) {
-      res.status(404).json({ success: false, error: 'File not found' });
-    } else {
-      // Respond with the content of the file
-      res.json(fileContent);
+    if( Student_ID !== undefined && Student_ID !== '' ){
+      fileContent = await s3service.getAllActivityByTestSessionAndStudentID(Test_Session, Student_ID);
+    }else{
+      fileContent = await s3service.getAllActivityByTestSession(Test_Session);
     }
+
+    if (fileContent === undefined || Array.isArray(fileContent) && fileContent.length === 0) res.status(404).json({ success: false, error: 'File not found' });
+    else res.json(fileContent);
+    
   } catch (error) {
     console.error('Error in S3 read controller:', error);
     res.status(500).json({ success: false, error: 'Internal Server Error' });
--- src/services/s3service.ts
+++ src/services/s3service.ts
@@ -3,6 +3,20 @@ import { S3 } from 'aws-sdk';
 import { Readable } from 'stream';
 import dotenv from 'dotenv';
 
+interface LogEntry {
+  Name: string;
+  Surname: string;
+  Activities: JSON[];
+}
+
+interface LogResponse {
+  Test_Session: string;
+  Student_ID: string;
+  Name: string;
+  Surname: string;
+  Activities: JSON[];
+}
+
 dotenv.config();
 
 const s3 = new S3({
@@ -10,52 +24,13 @@ const s3 = new S3({
   secretAccessKey: process.env.AWS_SECRET_KEY,
 });
 
-export const uploadToS3 = async (
-  test_Session: string, // Change to lowercase 'string'
-  student_ID: string, // Change to lowercase 'string'
-  log: JSON,
-): Promise<string> => {
-
-  try {
-    // Attempt to read the file from S3
-    let existingLog: JSON[] | undefined;
-    try {
-      existingLog = await readFileFromS3(test_Session, student_ID);
-    } catch (readError) {
-      // Handle the NoSuchKey error or log it if needed
-      console.error('Error reading file from S3:', readError);
-    }
-
-
-    // If the file exists, update the log list
-    if (existingLog && Array.isArray(existingLog)) {
-      existingLog.push(log);
-    } else {
-      // If the file doesn't exist or has invalid content, create a new list with the new log
-      existingLog = [log];
-    }
-
-    const params: S3.Types.PutObjectRequest = {
-      Bucket: process.env.BUCKET_NAME ?? 'test-forensic-backend',
-      Key: `${test_Session}/${student_ID}`,
-      Body: Readable.from(JSON.stringify(existingLog)),
-    };
-
-    const data = await s3.upload(params).promise();
-    return data.Location || '';
-  } catch (error) {
-    console.error('Error uploading to S3:', error);
-    throw error;
-  }
-};
-
-export const readFileFromS3 = async (
-  test_Session: string,
-  student_ID: string,
-): Promise<JSON[] | undefined> => {
+const getAllActivityByTestSessionAndStudentID = async (
+  Test_Session: string,
+  Student_ID: string,
+): Promise< LogResponse | undefined> => {
   const params: S3.Types.GetObjectRequest = {
-    Bucket: process.env.BUCKET_NAME ?? 'test-forensic-backend',
-    Key: `${test_Session}/${student_ID}`,
+    Bucket: process.env.BUCKET_NAME ?? '',
+    Key: `${Test_Session}/${Student_ID}`,
   };
 
   try {
@@ -65,8 +40,12 @@ export const readFileFromS3 = async (
     if (!jsonString) {
       throw new Error('Failed to convert Buffer to JSON string');
     }
+    let response: LogResponse;
+    response = JSON.parse(jsonString)
+    response.Test_Session = Test_Session
+    response.Student_ID = Student_ID
 
-    return JSON.parse(jsonString) as JSON[];
+    return response;
   } catch (error: any) {
     if (error.code === 'NoSuchKey') {
       console.error('File not found on S3:', error);
@@ -77,3 +56,98 @@ export const readFileFromS3 = async (
     }
   }
 };
+
+const getAllActivityByTestSession = async (
+  Test_Session: string,
+): Promise<LogResponse[]> => {
+  const params: S3.Types.ListObjectsV2Request = {
+    Bucket: process.env.BUCKET_NAME ?? '',
+    Prefix: `${Test_Session}/`,
+  };
+
+  try {
+    const data = await s3.listObjectsV2(params).promise();
+    const keys = data.Contents?.map((object) => object.Key || '') || [];
+
+    console.log(keys)
+    const logEntries: LogResponse[] = [];
+
+    // Loop through each key and retrieve the content
+    for (const key of keys) {
+      const [,studentId] = key.split('/'); // Assuming the format is "Test_Session/Student_ID"
+      try {
+        const logEntry = await getAllActivityByTestSessionAndStudentID(Test_Session, studentId);
+
+        if (logEntry) {
+          logEntries.push(logEntry);
+        }
+      } catch (readError) {
+        // Handle errors if needed
+        console.error(`Error reading file for key ${key} from S3:`, readError);
+      }
+    }
+
+    return logEntries;
+  } catch (error) {
+    console.error('Error listing keys from S3:', error);
+    throw error;
+  }
+};
+
+
+const uploadToS3 = async (
+  Test_Session: string,
+  Student_ID: string,
+  Name: string,
+  Surname: string,
+  log: JSON,
+): Promise<string> => {
+
+  try {
+    // Attempt to read the file from S3
+    let existingLog: LogResponse | undefined;
+    try {
+      existingLog = await getAllActivityByTestSessionAndStudentID(Test_Session, Student_ID);
+    } catch (readError) {
+      // Handle the NoSuchKey error or log it if needed
+      console.error('Error reading file from S3:', readError);
+    }
+
+    let LogData : LogEntry;
+
+    // If the file exists, update the log list
+    // if (existingLog && Array.isArray(existingLog)) {
+    if (existingLog) {
+      const {Test_Session, Student_ID, ...Data} = existingLog
+      LogData = Data
+      LogData.Activities.push(log);
+    } else {
+      // If the file doesn't exist or has invalid content, create a new list with the new log
+      LogData = {
+        Name: Name,
+        Surname: Surname,
+        Activities: [log]
+      };
+    }
+
+    const params: S3.Types.PutObjectRequest = {
+      Bucket: process.env.BUCKET_NAME ?? '',
+      Key: `${Test_Session}/${Student_ID}`,
+      Body: Readable.from(JSON.stringify(LogData)),
+    };
+
+    const data = await s3.upload(params).promise();
+    return data.Location || '';
+  } catch (error) {
+    console.error('Error uploading to S3:', error);
+    throw error;
+  }
+};
+
+
+
+export const s3service = {
+  uploadToS3, 
+  getAllActivityByTestSessionAndStudentID,
+  getAllActivityByTestSession
+}
